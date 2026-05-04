2dfe51359bd72e62b319e1b83129efb7bdf46da7
snehachourey <snehachourey2006@gmail.com>
2026-05-05 00:47:13 +0530

    backend changed

--- BACKEND/Data/questions.py
+++ BACKEND/Data/questions.py
@@ -1,6 +1,6 @@
 questions=[{"id":1,
            "question":"what do you enjoy most ?",
-           "options":["maths","bio","art","business"]},
+           "options":["maths","bio","art","bussness"]},
 
            {"id":2,
            "question":"what activity do you like ?",
